35b4375ee0bfd4589e95abc65a8dd3770b464fb1
天王州アテネ <qingcaomc@gmail.com>
2023-11-06 18:15:53 +0800

    network check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
+
+function CheckStatusCode() {
+  const [status, setStatus] = useState(null);
+
+    fetch('https://hdtv.neu6.edu.cn/v1/icon_256x256.913607b31dc70ad6dc0b29a132c6ce40.svg')
+      .then(response => {
+        setStatus(response.status);
+      })
+      .catch(error => {
+        console.error('请求失败:', error);
+        setStatus(null);
+      });
+
+  return (
+    <div>
+      {status == null && (
+        <p>❔Testing your network condition...</p>
+      )}
+      {status !== null && status == 200 && (
+        <p>✅You can now access kiwi sites! <br />
+          <a href='https://emby.kiwi.moe/' className='App-link'>🎬Emby</a> | <a href='https://drive.kiwi.moe/' className='App-link'>☁️Drive</a>
+        </p>
+      )}      
+      {status !== null && status !== 200 && (
+        <p>❌Your network does not support kiwi.</p>
+      )}
+    </div>
+  );
+}
+
 
 function App() {
   return (
@@ -8,6 +39,7 @@ function App() {
         <p>
           Kiwi.moe is now here!
         </p>
+        <CheckStatusCode />
       </header>
     </div>
   );
