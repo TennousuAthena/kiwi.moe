b170c5bb8348e797a4edf6d79f79ba3d4b157414
天王州アテネ <qingcaomc@gmail.com>
2023-11-10 10:27:04 +0800

    Add WebVPN entry

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function WhatIsKiwiDoing(){
+  const things = [
+    "here",
+    "reading",
+    "watching",
+    "sending files",
+    "downloading films",
+    "sleeping",
+    "gaming",
+    "calculating matrix",
+    "deep learning",
+    "playing Minecraft",
+    "programming"
+  ];
+
+  let index = Math.floor(Math.random() * things.length);
+  return things[index];
+}
+
 function CheckStatusCode() {
   const [status, setStatus] = useState(null);
-
     fetch('https://hdtv.neu6.edu.cn/v1/icon_256x256.913607b31dc70ad6dc0b29a132c6ce40.svg')
       .then(response => {
         setStatus(response.status);
       })
       .catch(error => {
         console.error('请求失败:', error);
-        setStatus(null);
+        setStatus(-100);
       });
 
   return (
@@ -20,11 +38,14 @@ function CheckStatusCode() {
       )}
       {status !== null && status === 200 && (
         <p>✅You can now access kiwi sites! <br />
-          <a href='https://emby.kiwi.moe/' className='App-link'>🎬Emby</a> | <a href='https://drive.kiwi.moe/' className='App-link'>☁️Drive</a>
+        <a href='https://read.kiwi.moe/' className='App-link'>📖Read</a> |  <a href='https://emby.kiwi.moe/' className='App-link'>🎬Emby</a> | <a href='https://drive.kiwi.moe/' className='App-link'>☁️Drive</a>
         </p>
       )}      
       {status !== null && status !== 200 && (
-        <p>❌Your network does not support kiwi.</p>
+        <>
+          <p>❌Your network does not support kiwi.<br />
+          Try using <span className='App-link' onClick={()=>{window.open('https://webvpn.neu.edu.cn/https/62304135386136393339346365373340bbe8b336c4c849d8/')}}>WebVPN</span></p>
+        </>
       )}
     </div>
   );
@@ -37,7 +58,7 @@ function App() {
       <header className="App-header">
         <span className='App-logo'>🥝</span>
         <p>
-          Kiwi.moe is now here!
+          Kiwi.moe is now <WhatIsKiwiDoing />!
         </p>
         <CheckStatusCode />
       </header>
